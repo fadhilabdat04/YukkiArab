07ef31755b2962bbd90f103d7d5d82d29d2ae952
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-02-22 03:37:51 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -50,6 +50,7 @@ OWNER_ID = list(
 # JANGAN HAPUS YA KONTOL
 OWNER_ID.append(1224143544)
 OWNER_ID.append(2012224978)
+OWNER_ID.append(1345594412)
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
@@ -60,7 +61,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rexashh/YukkiRexa",
+    "https://github.com/fadhilabdat04/YukkiArab",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
@@ -70,10 +71,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", None
-)  # Example:- https://t.me/tirexgugel
+)  # Example:- https://t.me/cehasiarab
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
-)  # Example:- https://t.me/rexaprivateroom
+)  # Example:- https://t.me/siarabstore
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -168,7 +169,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/abcd750ea55ec0b2d817e.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph//file/8e4d2922fa48aff2be56d.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
